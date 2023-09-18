684dae02be7ad8362d7631b3f3bed97e2881c969
ameyakore <kore.ameya@gmail.com>
2023-09-19 02:57:47 +0530

    feat: navigation pages done

---
+++ src/lib/FItemEntry.svelte
@@ -0,0 +1,13 @@
+<script>
+	import FileEntry from './FileEntry.svelte';
+	import FolderEntry from './FolderEntry.svelte';
+	export var fe;
+	export var path;
+
+	const fTypes = {
+		Folder: FolderEntry,
+		File: FileEntry,
+	};
+</script>
+
+<svelte:component this={fTypes[fe.entry_type]} {fe} {path} />
---
+++ src/lib/FileEntry.svelte
@@ -0,0 +1,36 @@
+<script>
+	import FileIcon from '../assets/file.svg';
+
+	export var fe;
+	export var path;
+</script>
+
+<div class="folder-item">
+	<div class="folder-item-row">
+		<img src={FileIcon} class="ficon" alt="file-icon" />
+		<div class="folder-item-text">{fe.name}</div>
+	</div>
+</div>
+
+<style>
+	.folder-item {
+		margin-left: 20px;
+	}
+
+	.folder-item-row {
+		display: flex;
+		flex-direction: row;
+		gap: 5px;
+		margin-top: 5px;
+		width: fit-content;
+		cursor: pointer;
+	}
+
+	.folder-item-row:hover {
+		text-decoration: underline;
+	}
+
+	.ficon {
+		align-self: center;
+	}
+</style>
